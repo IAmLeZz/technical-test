eea5ff7ec34a43bc79d711db9cc1c05d0f577770
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-19 03:49:46 -0300

    Display the new components Fill the components with the retrieved data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,3 +16,24 @@ export default function Home() {
   const { data: company, getSpaceXData: getCompany } = useSpaceXData({ endpoint: 'v4/company' })
   const { data: launches, getSpaceXData: getLaunches } = useSpaceXData({ endpoint: 'v5/launches' })
   const { data: payloads, getSpaceXData: getPayloads } = useSpaceXData({endpoint: 'v4/payloads'})
+
+  if (rockets && history && company) {
+    console.log(company)
+    return (
+      <main style={{ zIndex: '1' }}>
+        <ParticlesBG></ParticlesBG>
+        <h1 className='text-[40px] text-center font-bold company-text'>SPACEX APP</h1>
+       {  <Company spaceXData={company}></Company>  }
+        <h3 className='text-[30px] text-center font-bold'>Our rockets</h3>
+        <Rockets rockets={rockets} />
+        <History events={history}></History>
+        <div className='w-[100%] md:w-[45%] m-auto'>
+          <LaunchesChart launches={launches} isSuccess={true}></LaunchesChart>
+        </div>
+        <div className='w-[100%] md:w-[45%] h-auto m-auto'>
+          <PayloadsChart payload={payloads}></PayloadsChart>
+        </div>
+      </main>
+    )
+  }
+}
