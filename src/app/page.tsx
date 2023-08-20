98dd981e05a0476ecdfb69e393cba6a4b307649f
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-19 22:59:55 -0300

    Add new components and dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ import useSpaceXData from '@/hooks/useSpaceXData'
 import History from '@/components/History'
 import ParticlesBG from '@/components/ParticlesBG'
 import Company from '@/components/Company'
-import { useEffect, useState } from 'react'
-import EmployeesChart from '@/components/PayloadsChart'
 import LaunchesChart from '@/components/LaunchesChart'
 import PayloadsChart from '@/components/PayloadsChart'
+import Landpads from '@/components/Landpads'
+import Launchpads from '@/components/Launchpads'
+import useLightbox from '@/components/useLightbox'
+import slides from '@/components/slides'
+
+
 
 
 export default function Home() {
@@ -15,17 +19,19 @@ export default function Home() {
   const { data: history, getSpaceXData: getHistory } = useSpaceXData({ endpoint: 'v4/history' })
   const { data: company, getSpaceXData: getCompany } = useSpaceXData({ endpoint: 'v4/company' })
   const { data: launches, getSpaceXData: getLaunches } = useSpaceXData({ endpoint: 'v5/launches' })
-  const { data: payloads, getSpaceXData: getPayloads } = useSpaceXData({endpoint: 'v4/payloads'})
+  const { data: payloads, getSpaceXData: getPayloads } = useSpaceXData({ endpoint: 'v4/payloads' })
+  const { data: landpads, getSpaceXData: getLandpads } = useSpaceXData({ endpoint: 'v4/landpads' })
 
   if (rockets && history && company) {
-    console.log(company)
     return (
       <main style={{ zIndex: '1' }}>
         <ParticlesBG></ParticlesBG>
         <h1 className='text-[40px] text-center font-bold company-text'>SPACEX APP</h1>
-       {  <Company spaceXData={company}></Company>  }
+        { /* <Company spaceXData={company}></Company>  */}
         <h3 className='text-[30px] text-center font-bold'>Our rockets</h3>
-        <Rockets rockets={rockets} />
+        <Rockets rockets={rockets} /> 
+        <Landpads landpadsData={landpads}></Landpads>
+        <Launchpads></Launchpads>
         <History events={history}></History>
         <div className='w-[100%] md:w-[45%] m-auto'>
           <LaunchesChart launches={launches} isSuccess={true}></LaunchesChart>
