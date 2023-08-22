6a7641a06400d84e769065cf2282da6fe7626093
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-21 21:48:25 -0300

    Add images urls for Image component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,63 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.imgur.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'imgur.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'farm1.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'farm2.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'farm3.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'farm4.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'farm5.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'farm6.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'live.staticflickr.com',
+                port: '',
+                pathname: '/**',
+            }
+        ],
+    },
+}
 
 module.exports = nextConfig
