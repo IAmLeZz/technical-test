2ae4d85edb6cef99ea285d748e90afae4e4dabc3
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-21 21:49:43 -0300

    Fix types

--- src/services/rocketsNames.ts
+++ src/services/rocketsNames.ts
@@ -3,18 +3,18 @@ import { spaceX } from "./spaceX"
 export async function replaceRocketIdsWithNames(data: LaunchPad[]) {
     // Fetch rocket data for each rocket ID
     const rocketsData = await Promise.all(
-      data.flatMap((item: any) => item.rockets).map((rocketId: string) =>
+      data.flatMap((item: LaunchPad) => item.rockets).map((rocketId: string) =>
         spaceX(`v4/rockets/${rocketId}`)
       )
     )
   
     // Create a map of rocket IDs to rocket names
     const rocketsMap = Object.fromEntries(
-      rocketsData.map((rocket: any) => [rocket.id, rocket.name])
+      rocketsData.map((rocket: Rocket) => [rocket.id, rocket.name])
     )
   
     // Replace rocket IDs with rocket names
-    const updatedData: LaunchPad[] = data.map((item: any) => ({
+    const updatedData: LaunchPad[] = data.map((item: LaunchPad) => ({
       ...item,
       rockets: item.rockets.map((rocketId: string) => rocketsMap[rocketId]),
     }))
