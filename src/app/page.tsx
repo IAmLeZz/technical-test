f6b4f515f9d76e7aed672ee64ea16496604f3165
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-23 15:56:43 -0300

    Remove use client Move useSpaceXData calls to the components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 import Rockets from '@/components/Rockets'
 import useSpaceXData from '@/hooks/useSpaceXData'
 import History from '@/components/History'
@@ -12,34 +11,28 @@ import LandpadsChart from '@/components/LandpadsChart'
 
 
 export default function Home() {
-  const { data: rockets, getSpaceXData: getRockets } = useSpaceXData({ endpoint: 'v4/rockets' })
-  const { data: history, getSpaceXData: getHistory } = useSpaceXData({ endpoint: 'v4/history' })
-  const { data: company, getSpaceXData: getCompany } = useSpaceXData({ endpoint: 'v4/company' })
-  const { data: launches, getSpaceXData: getLaunches } = useSpaceXData({ endpoint: 'v5/launches' })
-  const { data: payloads, getSpaceXData: getPayloads } = useSpaceXData({ endpoint: 'v4/payloads' })
-  const { data: landpads, getSpaceXData: getLandpads } = useSpaceXData({ endpoint: 'v4/landpads' })
 
-  if (rockets && history && company) {
-    return (
-      <main style={{ zIndex: '1' }}>
-        <ParticlesBG></ParticlesBG>
-        <h1 className='text-[40px] text-center font-bold company-text'>SPACEX APP</h1>
-        {/* <Company spaceXData={company}></Company> */}
-        <h3 className='text-[30px] text-center font-bold'>Our rockets</h3>
-        <Rockets rockets={rockets} />
-        <Landpads landpadsData={landpads}></Landpads>
-        <Launchpads></Launchpads>
-        <History events={history}></History>
-        <div className='w-[100%] md:w-[45%] m-auto my-5'>
-          <LaunchesChart launches={launches} isSuccess={true}></LaunchesChart>
-        </div>
-        <div className='w-[100%] md:w-[45%] h-auto m-auto my-5'>
-          <PayloadsChart payload={payloads}></PayloadsChart>
-        </div>
-        <div className='w-[100%] md:w-[45%] h-auto m-auto my-5'>
-          <LandpadsChart landpadsData={landpads}></LandpadsChart>
-        </div>
-      </main>
-    )
-  }
+
+
+  return (
+    <main style={{ zIndex: '1' }}>
+      <ParticlesBG></ParticlesBG>
+      <h1 className='text-[40px] text-center font-bold company-text'>SPACEX APP</h1>
+      {/* <Company></Company> */}
+      <h3 className='text-[30px] text-center font-bold'>Our rockets</h3>
+      <Rockets />
+      <Landpads></Landpads>
+      <Launchpads></Launchpads>
+      <History></History>
+      <div className='w-[100%] md:w-[45%] m-auto my-5'>
+        <LaunchesChart isSuccess={true}></LaunchesChart>
+      </div>
+      <div className='w-[100%] md:w-[45%] h-auto m-auto my-5'>
+        <PayloadsChart></PayloadsChart>
+      </div>
+      <div className='w-[100%] md:w-[45%] h-auto m-auto my-5'>
+        <LandpadsChart></LandpadsChart>
+      </div>
+    </main>
+  )
 }
