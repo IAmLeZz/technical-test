8724cc9d39db9b081e8658a1cd10c8fd7f888df5
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-24 04:11:41 -0300

    Remove getRockets Rename spaceX to spaceXApiCaller

--- src/services/spaceX.ts
+++ src/services/spaceX.ts
@@ -1,7 +1,7 @@
 import { BASE_URL } from '@/utils/constants';
 import axios from 'axios';
 
-export const spaceX = async (endpoint: string, params?: Params) => {
+export const spaceXApiCaller = async (endpoint: string, params?: Params) => {
   try {
       const id = params?.id || '' ;
       const url = `${BASE_URL}/${endpoint}/${id}`;
@@ -12,28 +12,3 @@ export const spaceX = async (endpoint: string, params?: Params) => {
       console.log(error);
   }
 }
-
-export const getRockets = async () => {
-    try {
-      const rocketsResponse = await axios.get(`${BASE_URL}/rockets`);
-      const rocketsData = rocketsResponse.data;
-  
-      // Fetch additional data for each rocket
-      const rocketsWithImages = await Promise.all(
-        rocketsData.map(async (rocket: Rocket) => {
-          const imagesResponse = await axios.get(`${BASE_URL}/v4/rockets/${rocket.id}`);
-          const imagesData = imagesResponse.data;
-  
-          return {
-            ...rocket,
-            flickr_images: imagesData.flickr_images,
-          };
-        })
-      );
-  
-      return rocketsWithImages;
-    } catch (error) {
-      console.log(error);
-      return [];
-    }
-  };
