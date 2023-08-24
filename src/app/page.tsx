c6e428bcd225ffeb2c29d618b9befb9b3b025fdd
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-24 04:19:30 -0300

    Remove unnecessary imports and move text to Rockets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Rockets from '@/components/Rockets'
-import useSpaceXData from '@/hooks/useSpaceXData'
 import History from '@/components/History'
 import ParticlesBG from '@/components/ParticlesBG'
 import Company from '@/components/Company'
@@ -9,17 +8,12 @@ import Landpads from '@/components/Landpads'
 import Launchpads from '@/components/Launchpads'
 import LandpadsChart from '@/components/LandpadsChart'
 
-
 export default function Home() {
-
-
 
   return (
     <main style={{ zIndex: '1' }}>
       <ParticlesBG></ParticlesBG>
-      <h1 className='text-[40px] text-center font-bold company-text'>SPACEX APP</h1>
-      {/* <Company></Company> */}
-      <h3 className='text-[30px] text-center font-bold'>Our rockets</h3>
+      {<Company></Company>}
       <Rockets />
       <Landpads></Landpads>
       <Launchpads></Launchpads>
