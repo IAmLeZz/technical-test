d7a61d70d80739202ca22599ec61304ccbce08c5
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-24 04:16:11 -0300

    Change it to be a grid of multiple columns instead of one

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -9,23 +9,28 @@ const History = () => {
   const { data: historyData, error, loading } = useSpaceXData({ endpoint: 'v4/history' })
   useEffect(() => {
     async function updateEventData() {
-      setEvents(historyData)
+      if (historyData) {
+        setEvents(historyData)
+      } else {
+        error
+      }
     }
     updateEventData()
-  }, [historyData])
+  }, [historyData, error])
+
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error}</div>
   return (
     <div className="py-10">
       <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <h1 className="text-3xl font-extrabold text-gray-200 mb-8">Events we are proud of</h1>
-        <ul className="space-y-8">
+        <ul className="grid md:grid-cols-4 md:grid-rows-2 md:gap-6 grid-cols-1 gap-6">
           {events.map((event) => (
             <li key={event.id}>
               <div className="p-6 bg-white rounded-lg event-box">
                 <h2 className="text-xl font-semibold text-gray-800 mb-2">{event.title}</h2>
                 <p className="text-gray-600">{new Date(event.event_date_utc).toLocaleDateString()}</p>
-                <p className="text-gray-700 mt-2">{event.details}</p>
+                <p className="text-gray-700 mt-2 description">{event.details}</p>
                 <a
                   href={event.links.article}
                   target="_blank"
