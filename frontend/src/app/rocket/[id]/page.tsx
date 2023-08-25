9bb20971561623d8cb844c1323d2274d48e460af
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-25 03:43:33 -0300

    Fix import

--- frontend/src/app/rocket/[id]/page.tsx
+++ frontend/src/app/rocket/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { PageProps } from '../../../../.next/types/app/layout';
 import Rocket from './Rocket';
 import ParticlesBG from '@/components/ParticlesBackground';
+import { PageProps } from '../../../../.next/types/app/rocket/[id]/page';
 
 export default function Page({ params }: PageProps) {
     return (
