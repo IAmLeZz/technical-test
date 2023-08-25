2f9ac1e8f124cc6cd4438f4b376bf8c8889160b0
Alvaro Villalo <alvaro_villalo@hotmail.com>
2023-08-25 01:43:42 -0300

    Fix imports Add Notification component Clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,33 @@
 import Rockets from '@/components/Rockets'
 import History from '@/components/History'
-import ParticlesBG from '@/components/ParticlesBG'
 import Company from '@/components/Company'
-import LaunchesChart from '@/components/LaunchesChart'
-import PayloadsChart from '@/components/PayloadsChart'
+import LaunchesChart from '@/components/charts/LaunchesChart'
+import PayloadsChart from '@/components/charts/PayloadsChart'
 import Landpads from '@/components/Landpads'
 import Launchpads from '@/components/Launchpads'
-import LandpadsChart from '@/components/LandpadsChart'
+import Notification from '@/components/Notification'
+import LandpadsChart from '@/components/charts/LandpadsChart'
+import ParticlesBackground from '@/components/ParticlesBackground'
 
 export default function Home() {
 
   return (
     <main style={{ zIndex: '1' }}>
-      <ParticlesBG></ParticlesBG>
-      {<Company></Company>}
+      <Notification />
+      <ParticlesBackground />
+      <Company />
       <Rockets />
-      <Landpads></Landpads>
-      <Launchpads></Launchpads>
-      <History></History>
+      <Landpads />
+      <Launchpads />
+      <History />
       <div className='w-[100%] md:w-[45%] m-auto my-5'>
-        <LaunchesChart isSuccess={true}></LaunchesChart>
+        <LaunchesChart isSuccess={true} />
       </div>
       <div className='w-[100%] md:w-[45%] h-auto m-auto my-5'>
-        <PayloadsChart></PayloadsChart>
+        <PayloadsChart />
       </div>
       <div className='w-[100%] md:w-[45%] h-auto m-auto my-5'>
-        <LandpadsChart></LandpadsChart>
+        <LandpadsChart />
       </div>
     </main>
   )
